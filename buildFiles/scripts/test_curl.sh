30235963cef90a21dccdb357494c74b1d016112e
Milan Adamov <madamov@mac.com>
2024-03-20 17:28:35 +0100

    more debugging

--- buildFiles/scripts/test_curl.sh
+++ buildFiles/scripts/test_curl.sh
@@ -27,3 +27,7 @@ which curl
 curl -k -u ${UPLOAD_USER}:${UPLOAD_PASSWORD} -T $HOME/Documents/test.txt ${uploadURL}test_001.txt
 
 curl -k -u ${TEST_USER}:${TEST_PASSWORD} -T $HOME/Documents/test.txt ${uploadURL}test_002.txt
+
+echo "SFTP_USER je $SFTP_USER"
+
+curl -k -u ${SFTP_USER}:${SFTP_PASSWORD} -T $HOME/Documents/test.txt ${uploadURL}test_002.txt

fa44bc9229917aab07cfde7370d9a8e8e719e01c
Milan Adamov <madamov@mac.com>
2024-03-20 17:05:46 +0100

    removed / from the end

--- buildFiles/scripts/test_curl.sh
+++ buildFiles/scripts/test_curl.sh
@@ -11,7 +11,7 @@ ls -al /usr/local/opt/ > $HOME/Documents/test.txt
 
 echo "Test text" >> $HOME/Documents/test.txt
 
-curl -k -u builder4d:idolize-roof-scud-rigour -T $HOME/Documents/test.txt sftp://updates.4d.rs/home/builder4d/updates.4d.rs/appBuilder/log4D.txt
+curl -k -u builder4d:idolize-roof-scud-rigour -T $HOME/Documents/test.txt sftp://updates.4d.rs/home/builder4d/updates.4d.rs/appBuilder/testdirektno.txt
 
 
 # echo "Using Homebrew curl"
@@ -22,5 +22,5 @@ echo "Using curl, path je: "
 
 which curl
 
-curl -k -u ${UPLOAD_USER}:${UPLOAD_PASSWORD} --ftp-create-dirs -T $HOME/Documents/test.txt ${uploadURL}/test_001.txt
+curl -k -u ${UPLOAD_USER}:${UPLOAD_PASSWORD} -T $HOME/Documents/test.txt ${uploadURL}test_001.txt
 

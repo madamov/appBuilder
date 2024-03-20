c29129c2ffd1462040e906b6499dcc303e68d3b4
Milan Adamov <madamov@mac.com>
2024-03-20 15:47:41 +0100

    fixed env vars for uoloading

--- buildFiles/scripts/test_curl.sh
+++ buildFiles/scripts/test_curl.sh
@@ -7,17 +7,17 @@ uploadURL=$(jq -r '.uploadCompiledStruct' $workingDirectory/buildFiles/parameter
 echo "User je $UPLOAD_USER"
 echo "URL je $uploadURL"
 
-ls -al /usr/local/opt/
+ls -al /usr/local/opt/ > $HOME/Documents/test.txt
 
-echo "Test text" > $HOME/Documents/test.txt
+echo "Test text" >> $HOME/Documents/test.txt
  
 echo "Using Homebrew curl"
  
-/usr/local/opt/curl/bin/curl -k -u ${UPLOAD_USER}:${UPLOAD_PASSWORD} --ftp-create-dirs -T $HOME/Documents/test.txt ${myStructURL}/test_001.txt
+/usr/local/opt/curl/bin/curl -k -u ${UPLOAD_USER}:${UPLOAD_PASSWORD} --ftp-create-dirs -T $HOME/Documents/test.txt ${uploadURL}/test_001.txt
 
 echo "Using curl"
 
 which curl
 
-curl -k -u ${UPLOAD_USER}:${UPLOAD_PASSWORD} --ftp-create-dirs -T $HOME/Documents/test.txt ${myStructURL}/test_001.txt
+curl -k -u ${UPLOAD_USER}:${UPLOAD_PASSWORD} --ftp-create-dirs -T $HOME/Documents/test.txt ${uploadURL}/test_001.txt
 

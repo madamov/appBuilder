f6defdf8abcb074d4798e4bf00d608abc4f237de
Milan Adamov <madamov@mac.com>
2024-03-26 20:30:53 +0100

    modified some scripts

--- buildFiles/scripts/set_upload_user_credentials.sh
+++ buildFiles/scripts/set_upload_user_credentials.sh
@@ -18,20 +18,14 @@
          fi
          if [ "$(basename $GITHUB_REF)" == "develop" ]; then
             # Use test server for uploads
-            # echo Using test server parameters for branch develop
-            # mv ./buildFiles/parameters_testserver.json ./buildFiles/parameters.json
-            # export UPLOAD_USER=${{secrets.UPLOAD_TEST_USER}}
-            # export UPLOAD_PASSWORD=${{secrets.UPLOAD_TEST_PASSWORD}}
-            
-            # USE CXR NIGHTLY BUILD SERVER FOR TESTING, MAIN BRANCH WILL DO THAT ALSO
-            echo nightly builds folder choosen
-            mv ./buildFiles/parameters_nightly.json ./buildFiles/parameters.json
-            export UPLOAD_USER=${{secrets.UPLOAD_USER}}
-            export UPLOAD_PASSWORD=${{secrets.UPLOAD_PASSWORD}}
+            echo "Using test server parameters for branch develop"
+            mv ./buildFiles/parameters_testserver.json ./buildFiles/parameters.json
+            export UPLOAD_USER=${{secrets.UPLOAD_TEST_USER}}
+            export UPLOAD_PASSWORD=${{secrets.UPLOAD_TEST_PASSWORD}}
+    
          fi
          if [ "$(basename $GITHUB_REF)" == "main" ]; then
             # Use default paramaters.json
             export UPLOAD_USER=${{secrets.UPLOAD_USER}}
             export UPLOAD_PASSWORD=${{secrets.UPLOAD_PASSWORD}}
          fi 
-         

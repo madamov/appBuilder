716ca5a17c6a81c91f6dad53c7b36f8c456bad20
Milan Adamov <madamov@mac.com>
2024-04-26 22:47:44 +0200

    get licenses from sercret

--- buildFiles/scripts/licenses.sh
+++ buildFiles/scripts/licenses.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# script gets licenses form secrets, expands them and copies them so 
+# script downloads licenses, expands them and copies them so 
 # 4D will run as Development Professional allowing us to compile and build
 
 workingDirectory=$(pwd)
 
+myurl=$(echo $1 | sed 's/https:\/\//&'"$BINARIES_USER:$BINARIES_PASSWORD@/")
+
+# curl -s -f -o $HOME/Documents/dev_lic.zip $myurl
+
+echo "🐚:🐚:: Licenses downloaded"
+
 # get license from secrets
 
 echo -n "$DEV_LIC" | base64 --decode -o $HOME/Documents/dev_lic.zip
-
-echo "🐚:🐚:: Licenses extracted"
 
 if [ -d "$HOME/Documents/Licenses" ]; then
 	echo "🐚:🐚:: Licenses in Documents exists"
@@ -35,6 +39,5 @@ fi
 
 # copy licenses so 4D is licensed as Developer Professional
 # leave originals in Documents folder, we will refer to them in buildApp.4DSettings XML file
-
 cp $HOME/Documents/Licenses/* $HOME/Library/Application\ Support/4D/Licenses/
 echo "🐚:🐚:: Licenses copied to " $HOME/Library/Application\ Support/4D/Licenses/

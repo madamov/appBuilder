8c84dcce0ebd29fd44ffe1d2d87037aac66f7301
Milan Adamov <madamov@mac.com>
2024-04-27 22:53:36 +0200

    fixed downloading of macOS 4d server

--- buildFiles/scripts/get4DServer.sh
+++ buildFiles/scripts/get4DServer.sh
@@ -2,9 +2,11 @@
 
 workingDirectory=$(pwd)
 
+myurl=$(echo $1 | sed 's/https:\/\//&'"$BINARIES_USER:$BINARIES_PASSWORD@/")
+
 echo "🐚:🐚: Downloading 4D ..."
 # download 4D
-curl -s -o -f $HOME/Documents/4D_Server.zip $1
+curl -s -o -f $HOME/Documents/4D_Server.zip $myurl
 echo "🐚:🐚: 4D Server $1 downloaded, unzipping archive ..."
 unzip -q $HOME/Documents/4D_Server.zip -d $HOME/Documents/
 echo "🐚🐚:: 4D Server unzipped"

15c3b9e602fae01c6e3250c54be82bb0f67142b9
Milan Adamov <madamov@mac.com>
2024-04-28 00:34:58 +0200

    fixed typo

--- buildFiles/scripts/release_client.sh
+++ buildFiles/scripts/release_client.sh
@@ -25,7 +25,7 @@ cd $destinationFolder
 # mv Client\ server\ executable final_app
 
 mkdir $destinationFolder/Client
-mv "$destinationFolder/Client\ Server\ executable/{appName}\ Client.app" "$destinationFolder/Client/${appName}\ Client.app"
+mv "$destinationFolder/Client\ Server\ executable/${appName}\ Client.app" "$destinationFolder/Client/${appName}\ Client.app"
 myAppDest="$destinationFolder/Client"
 	
 if [ -z "$uploadURL" ]; then

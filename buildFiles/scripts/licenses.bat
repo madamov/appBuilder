ef3bff3f16b2ccee8663134db57d099372343f87
Milan Adamov <madamov@mac.com>
2024-04-27 16:51:50 +0200

    corrected dev license handling

--- buildFiles/scripts/licenses.bat
+++ buildFiles/scripts/licenses.bat
@@ -1,12 +1,12 @@
 rem GET DEVELOPER LICENSES
 
-echo "getting licenses from %1%"
+rem echo "getting licenses from %1%"
 
 rem curl -u %BINARIES_USER%:%BINARIES_PASSWORD% -o %HOMEDRIVE%%HOMEPATH%\Downloads\dev_lic.zip %1%
 
 mkdir %HOMEDRIVE%%HOMEPATH%\Documents\Licenses
 
-powershell -command "Set-Content -Path %HOMEDRIVE%%HOMEPATH%\Downloads\dev_lic.tb64 -Value $env:DEV_LIC"
+powershell -command "Set-Content -Path %HOMEDRIVE%%HOMEPATH%\Downloads\dev_lic.b64 -Value $env:DEV_LIC"
 
 certutil -decode %HOMEDRIVE%%HOMEPATH%\Downloads\dev_lic.b64 %HOMEDRIVE%%HOMEPATH%\Downloads\dev_lic.zip
 
@@ -21,4 +21,4 @@ mkdir %ProgramData%\4D\Licenses
 
 copy /Y %HOMEDRIVE%%HOMEPATH%\Documents\Licenses\*.* %ProgramData%\4D\Licenses\
 
-dir /s %ProgramData%\4D\Licenses\*.* > %HOMEDRIVE%%HOMEPATH%\Documents\artifacts\licensedir1.txt
+dir /s %ProgramData%\4D\Licenses\*.* > %HOMEDRIVE%%HOMEPATH%\Documents\artifacts\licensedir3.txt

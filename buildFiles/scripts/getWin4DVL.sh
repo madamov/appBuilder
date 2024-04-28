4000eb494ea5664ff3d515ddec820f6e8645962f
Milan Adamov <madamov@mac.com>
2024-04-28 13:56:55 +0200

    correct file name

--- buildFiles/scripts/getWin4DVL.sh
+++ buildFiles/scripts/getWin4DVL.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# download 4D Volume Desktop for Windows on macOS, leave it in Documents folder, we will refer to it in buildApp.4DSettings XML file
+# download 4D Volume Desktop for Windows, leave it in Documents folder, we will refer to it in buildApp.4DSettings XML file
 
 workingDirectory=$(pwd)
 
 myurl=$(echo $1 | sed 's/https:\/\//&'"$BINARIES_USER:$BINARIES_PASSWORD@/")
 
 echo "🐚:🐚: Downloading 4D Volume Desktop ..."
-curl -s -o $HOME/Documents/4D_VL_WIN.zip $myurl
+curl -s -o $HOME/Documents/4D_VL.zip $myurl
 echo "🐚:🐚: 4D Volume Desktop $1 downloaded, unzipping archive ..."
-mkdir $HOME/Documents/4D_VL_WIN
-unzip -q $HOME/Documents/4D_VL_WIN.zip -d $HOME/Documents/4D_VL_WIN
+unzip -q $HOME/Documents/4D_VL_WIN -d $HOME/Documents/
 echo "🐚:🐚: 4D Volume Desktop FOR WINDOWS unzipped"

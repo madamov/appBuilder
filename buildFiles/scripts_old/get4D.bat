e2ab38f4c556e169314a343eeac2df9184c6128b
Milan Adamov <madamov@mac.com>
2024-04-29 17:08:33 +0200

    removed old and unused scripts

--- buildFiles/scripts_old/get4D.bat
+++
@@ -1,9 +0,0 @@
-rem download 4D, leave it in Documents\4D folder
-
-
-echo "BAT: Downloading 4D for Windows ..."
-curl -s -u %BINARIES_USER%:%BINARIES_PASSWORD% -o %HOMEDRIVE%%HOMEPATH%\Downloads\4D.zip %1%
-echo "BAT: 4D Windows downloaded, unzipping archive ..."
-mkdir %HOMEDRIVE%%HOMEPATH%\Documents\4D
-%sevenzip% x %HOMEDRIVE%%HOMEPATH%\Downloads\4D.zip -o%HOMEDRIVE%%HOMEPATH%\Documents\4D\
-echo "BAT: 4D unzipped"
